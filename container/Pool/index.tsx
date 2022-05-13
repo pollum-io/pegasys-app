c68c3d707fd706d0d086d1eca8d3d0320b9795c1
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2022-05-12 21:02:41 -0300

    wip: pool page

---
+++ container/Pool/index.tsx
@@ -0,0 +1,121 @@
+import { Button, Flex, Img, Link, Text } from '@chakra-ui/react';
+import { DefaultTemplate } from 'container';
+import { usePicasso } from 'hooks';
+import { NextPage } from 'next';
+
+export const PoolContainer: NextPage = () => {
+	const theme = usePicasso();
+	return (
+		<DefaultTemplate>
+			<Flex
+				w="100%"
+				h="100%"
+				alignItems="flex-start"
+				justifyContent="center"
+				pt="24"
+			>
+				<Flex flexDirection="column" maxW="xl">
+					<Flex
+						flexDirection="column"
+						zIndex="docked"
+						position="relative"
+						borderRadius="xl"
+						backgroundColor="blue.700"
+					>
+						<Img
+							borderRadius="xl"
+							src="images/backgrounds/1.png"
+							position="absolute"
+							zIndex="base"
+							w="100%"
+							h="100%"
+							objectFit="none"
+							opacity="0.4"
+							objectPosition="25% 20%"
+						/>
+						<Flex zIndex="docked" flexDirection="column" px="4" py="4" gap="3">
+							<Text fontWeight="medium">Liquidity provider rewards</Text>
+							<Text fontWeight="medium" fontSize="sm">
+								Liquidity providers earn a 0.25% fee on all trades proportional
+								to their share of the pool. Fees are added to the pool, accrue
+								in real time and can be claimed by withdrawing your liquidity.
+							</Text>
+						</Flex>
+					</Flex>
+					<Flex
+						alignItems="flex-start"
+						my="8"
+						justifyContent="flex-start"
+						w="100%"
+						flexDirection="column"
+					>
+						<Link
+							href="https://info.pegasys.finance/accounts"
+							color={theme.text.mono}
+							textDecoration="underline"
+							fontSize="lg"
+							fontWeight="medium"
+						>
+							View your staked liquidity
+						</Link>
+						<Flex
+							mt="4"
+							flexDirection="row"
+							justifyContent="space-between"
+							w="100%"
+						>
+							<Text fontSize="20" fontWeight="medium">
+								Your liquidity
+							</Text>
+							<Flex gap="4">
+								<Button
+									fontSize="14"
+									py="2"
+									px="2"
+									h="max-content"
+									bgColor="transparent"
+									borderWidth="1px"
+									borderColor={theme.bg.button.primary}
+									opacity="0.8"
+									_hover={{ opacity: '1' }}
+									_active={{}}
+								>
+									Create a pair
+								</Button>
+								<Button
+									fontSize="14"
+									py="2"
+									px="2"
+									h="max-content"
+									bgColor={theme.bg.button.primary}
+									color="white"
+									opacity="0.8"
+									_hover={{ opacity: '1' }}
+									_active={{}}
+								>
+									Add liquidity
+								</Button>
+							</Flex>
+						</Flex>
+					</Flex>
+					<Flex
+						w="100%"
+						flexDirection="column"
+						alignItems="center"
+						justifyContent="center"
+						fontWeight="normal"
+						gap="16"
+					>
+						<Text color={theme.text.mono} opacity="0.6" w="max-content">
+							Connect a wallet to view your liquidity.
+						</Text>
+						<Text color={theme.text.mono} fontSize="sm" w="max-content">
+							Don't see a pool you joined?{' '}
+							<Link fontWeight="medium">Import it.</Link>
+						</Text>
+					</Flex>
+				</Flex>
+			</Flex>
+		</DefaultTemplate>
+	);
+};
---
+++ pages/pool.tsx
@@ -0,0 +1,8 @@
+import { PoolContainer } from 'container';
+import type { NextPage } from 'next';
+
+const Pool: NextPage = () => {
+	return <PoolContainer />;
+};
+
+export default Pool;
