9a197257ddb5450bd39547242e716863b353d722
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2022-07-26 18:06:25 -0300

    feat: logo animation on loading modal

--- container/Pools/index.tsx
+++ container/Pools/index.tsx
@@ -14,6 +14,7 @@ import {
 } from "@chakra-ui/react";
 import { AddLiquidityModal, RemoveLiquidity } from "components";
 import { ImportPoolModal } from "components/Modals/ImportPool";
+import { LoadingTransition } from "components/Modals/LoadingTransition";
 import { PoolCards } from "components/Pools/PoolCards";
 import { usePicasso, useWallet } from "hooks";
 import { NextPage } from "next";
@@ -58,6 +59,7 @@ export const PoolsContainer: NextPage = () => {
 				isCreate={isCreate}
 				haveValue={haveValue}
 			/>
+			<LoadingTransition isOpen={isOpen} onClose={onClose} />
 			<Flex alignItems="flex-start" justifyContent="center" pt="20" mb="6.2rem">
 				<Flex flexDirection="column" w="2xl">
 					<Flex
