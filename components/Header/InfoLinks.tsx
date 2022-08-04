cff792a32d1f0abe6451416598488b7e6a63b885
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2022-08-04 09:12:25 -0300

    fix: info modal  responsiveness

--- components/Header/InfoLinks.tsx
+++ components/Header/InfoLinks.tsx
@@ -16,7 +16,8 @@ export const InfoLinks: FunctionComponent<ILinkProps> = props => {
 			fontSize="md"
 			fontWeight="normal"
 			color={theme.text.mono}
-			p="2"
+			px={["2", "2", "2", "2"]}
+			py={["3", "3", "2", "2"]}
 			_hover={{ color: theme.text.cyan }}
 			isExternal={!isVote}
 			{...rest}
