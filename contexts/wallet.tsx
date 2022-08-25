52c933f19c1beef5c099a34bb7e1c03f84fd6557
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-08-25 14:17:00 -0300

    fix: add type in tx object

--- contexts/wallet.tsx
+++ contexts/wallet.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, createContext, useState, useMemo } from "react";
 import { ethers, Signer } from "ethers";
 import { convertHexToNumber, isAddress } from "utils";
 import { AbstractConnector } from "@web3-react/abstract-connector";
-import { IWalletInfo } from "types";
+import { IWalletInfo, ITransactionResponse } from "types";
 import { useToasty } from "hooks";
 import {
 	INITIAL_ALLOWED_SLIPPAGE,
@@ -20,6 +20,14 @@ export enum ApprovalState {
 export interface IApprovalState {
 	status: ApprovalState;
 	type: string;
+}
+interface ITx {
+	57: {
+		[hash: string]: ITransactionResponse;
+	};
+	5700: {
+		[hash: string]: ITransactionResponse;
+	};
 }
 interface IWeb3 {
 	isConnected: boolean;
@@ -48,8 +56,8 @@ interface IWeb3 {
 	setOtherWallet: React.Dispatch<React.SetStateAction<boolean>>;
 	userSlippageTolerance: number;
 	setUserSlippageTolerance: React.Dispatch<React.SetStateAction<number>>;
-	setTransactions: React.Dispatch<React.SetStateAction<object>>;
-	transactions: object;
+	setTransactions: React.Dispatch<React.SetStateAction<ITx>>;
+	transactions: ITx;
 	setApprovalState: React.Dispatch<React.SetStateAction<IApprovalState>>;
 	approvalState: IApprovalState;
 }
@@ -79,7 +87,7 @@ export const WalletProvider: React.FC<{ children: React.ReactNode }> = ({
 	const [userSlippageTolerance, setUserSlippageTolerance] = useState<number>(
 		INITIAL_ALLOWED_SLIPPAGE
 	);
-	const [transactions, setTransactions] = useState<object>({
+	const [transactions, setTransactions] = useState<ITx>({
 		57: {},
 		5700: {},
 	});
@@ -176,10 +184,12 @@ export const WalletProvider: React.FC<{ children: React.ReactNode }> = ({
 						) {
 							setTransactions({
 								...transactions,
-								[currentNetworkChainId]: {
-									...transactions[currentNetworkChainId],
+								[Number(currentNetworkChainId)]: {
+									...transactions[currentNetworkChainId === 57 ? 57 : 5700],
 									[hash]: {
-										...transactions[currentNetworkChainId][hash],
+										...transactions[currentNetworkChainId === 57 ? 57 : 5700][
+											hash
+										],
 										...result,
 										hash,
 									},
