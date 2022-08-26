6f73df4bb75986f6675c1dd7affe9b23177db51d
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2022-08-26 12:21:22 -0300

    fix: getTokenAllowance type errors

--- utils/getTokenAllowance.ts
+++ utils/getTokenAllowance.ts
@@ -1,5 +1,5 @@
 import { Token, TokenAmount } from "@pollum-io/pegasys-sdk";
-import { Signer } from "ethers";
+import { Signer, Contract } from "ethers";
 import { getContract, singleCall } from "utils";
 
 export async function getTokenAllowance(
@@ -7,11 +7,11 @@ export async function getTokenAllowance(
 	owner: string,
 	spender: string,
 	signer: Signer
-): TokenAmount | undefined {
+): Promise<TokenAmount | undefined> {
 	const contract = await getContract(token?.address, signer);
 
 	const inputs = [owner, spender];
-	const allowance = await singleCall(contract, "allowance");
+	const allowance = await singleCall(contract as Contract, "allowance");
 	const allowanceValue = await allowance(inputs[0], inputs[1]);
 
 	return token && allowance
