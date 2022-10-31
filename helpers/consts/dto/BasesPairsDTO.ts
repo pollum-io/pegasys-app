32c57c2c1f22aee023d1e39a9a7294c3474ae595
Leonardo Faleiros <leonardo.faleiros@pollum.io>
2022-10-31 11:12:48 -0300

    feat: Finishing consts values and set it to review

--- helpers/consts/dto/BasesPairsDTO.ts
+++ helpers/consts/dto/BasesPairsDTO.ts
@@ -3,3 +3,7 @@ import { ChainId, Token } from "@pollum-io/pegasys-sdk";
 export type IBasePairsDTO = {
 	readonly [chainId in ChainId]: Token[];
 };
+
+export type ICustomBasePairDTO = {
+	[chainId in ChainId]?: { [tokenAddress: string]: Token[] };
+};
--- helpers/consts/index.ts
+++ helpers/consts/index.ts
@@ -1,3 +1,5 @@
 export * from "./contractRouters";
 export * from "./tokens";
 export * from "./basesPairs";
+export * from "./networks";
+export * from "./tokenListInfos";
