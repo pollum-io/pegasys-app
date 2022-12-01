bbdf419f4a1e2163f357b56406ac64780534e01f
João Cyrino <joaopedrocancado@gmail.com>
2022-11-30 22:53:21 -0300

    remove stake provider on default template

--- container/DefaultTemplate/index.tsx
+++ container/DefaultTemplate/index.tsx
@@ -10,7 +10,7 @@ import {
 import { usePicasso } from "hooks";
 import { getLibrary } from "utils";
 import { ModalsProvider } from "contexts/modals";
-import { PegasysProvider, StakeProvider } from "pegasys-services";
+import { PegasysProvider } from "pegasys-services";
 
 interface BaseLayoutProps {
 	children?: ReactNode;
@@ -48,78 +48,74 @@ export const DefaultTemplate: FunctionComponent<BaseLayoutProps> = ({
 					<TokensListManageProvider>
 						<TokensProvider>
 							<ModalsProvider>
-								<StakeProvider>
-									<div
+								<div
+									style={{
+										width: "100vw",
+										height: "100vh",
+										overflow: "auto",
+									}}
+									id={className}
+								>
+									<Flex
+										bgColor={theme.bg.primary}
+										flexDirection={["column"]}
 										style={{
-											width: "100vw",
-											height: "100vh",
-											overflow: "auto",
+											minHeight: heightValue,
+											width: widthValue,
 										}}
-										id={className}
+										alignItems="center"
 									>
 										<Flex
-											bgColor={theme.bg.primary}
-											flexDirection={["column"]}
-											style={{
-												minHeight: heightValue,
-												width: widthValue,
-											}}
-											alignItems="center"
-										>
-											<Flex
-												width="100%"
-												height={["100%", "100%", "40%", "40%"]}
-												margin="0 auto"
-												top={["-20rem", "-20rem", "-5rem", "-5rem"]}
-												position="fixed"
-												background={
-													colorMode === "dark"
-														? "radial-gradient(ellipse at center, #56BED8, #010101)"
-														: "radial-gradient(ellipse at center, #68e1ffbe, #e6faff)"
-												}
-												filter="blur(175px)"
-											/>
-											{colorMode === "dark" ? (
-												<Flex>
-													{colorMode === "dark" ? (
-														<Flex>
-															<div id="starsLightMode" />
-															<div id="starsLightMode2" />
-															<div id="starsLightMode3" />
-														</Flex>
-													) : (
-														<Flex>
-															<div id="stars" />
-															<div id="stars2" />
-															<div id="stars3" />
-														</Flex>
-													)}
-												</Flex>
-											) : (
-												<Flex>
-													<div id="stars" />
-													<div id="stars2" />
-													<div id="stars3" />
-												</Flex>
-											)}
+											width="100%"
+											height={["100%", "100%", "40%", "40%"]}
+											margin="0 auto"
+											top={["-20rem", "-20rem", "-5rem", "-5rem"]}
+											position="fixed"
+											background={
+												colorMode === "dark"
+													? "radial-gradient(ellipse at center, #56BED8, #010101)"
+													: "radial-gradient(ellipse at center, #68e1ffbe, #e6faff)"
+											}
+											filter="blur(175px)"
+										/>
+										{colorMode === "dark" ? (
+											<Flex>
+												{colorMode === "dark" ? (
+													<Flex>
+														<div id="starsLightMode" />
+														<div id="starsLightMode2" />
+														<div id="starsLightMode3" />
+													</Flex>
+												) : (
+													<Flex>
+														<div id="stars" />
+														<div id="stars2" />
+														<div id="stars3" />
+													</Flex>
+												)}
+											</Flex>
+										) : (
+											<Flex>
+												<div id="stars" />
+												<div id="stars2" />
+												<div id="stars3" />
+											</Flex>
+										)}
 
-											<Header />
-											<Flex
-												id="footer-blur"
-												w="100vw"
-												h="9%"
-												position="fixed"
-												bottom={["25", "25", "-5", "-5"]}
-												background={
-													colorMode === "dark" ? "#03060c" : "#ffffff"
-												}
-												zIndex="40"
-												filter="blur(35px)"
-											/>
-											{children}
-										</Flex>
-									</div>
-								</StakeProvider>
+										<Header />
+										<Flex
+											id="footer-blur"
+											w="100vw"
+											h="9%"
+											position="fixed"
+											bottom={["25", "25", "-5", "-5"]}
+											background={colorMode === "dark" ? "#03060c" : "#ffffff"}
+											zIndex="40"
+											filter="blur(35px)"
+										/>
+										{children}
+									</Flex>
+								</div>
 							</ModalsProvider>
 						</TokensProvider>
 					</TokensListManageProvider>
